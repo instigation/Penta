3e63957d778abc79c0046c1d269b5d9ea6b0cda5
instigation <instigation@naver.com>
2017-08-27 00:03:44 +0900

    on-screen button 보이게 함. on-screen button이 아니라 navigation bar로 검색하니까 나오네

---
+++ Assets/Scripts/Renderer/NavigationBarShower.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigationBarShower : MonoBehaviour {
+
+    // https://forum.unity3d.com/threads/disable-immersivemode-unity5.313911/
+    void Awake () {
+        Screen.fullScreen = false;
+    }
+}
